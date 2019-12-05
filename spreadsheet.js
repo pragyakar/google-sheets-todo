0d22d663bea56956c60da88601ec836b86c677e5
pragyakarfuse <pragyakar@fusemachines.com>
2019-12-05 14:05:46 +0545

    :wrench: Segregated sheet properties

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,6 +1,7 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
 const creds = require('./client_secret.json');
+const sheet_props = require('./sheet_properties.json');
 
 getId = (fullId) => {
   const idArray = fullId.split('/');
@@ -16,13 +17,13 @@ printTodo = (todo) => {
 }
 
 accessSpreadsheet = async () => {
-  const doc = new GoogleSpreadsheet('1KypeAsKIdpZU7TI7GNVj4YEY-rSQ851Aqsp3c_2VaFw');
+  const doc = new GoogleSpreadsheet(sheet_props.sheet_id);
   await promisify(doc.useServiceAccountAuth)(creds);
   const info = await promisify(doc.getInfo)();
   const sheet = info.worksheets[0];
   
   const rows  = await promisify(sheet.getRows) ({
-    offset: 1,
+    offset: sheet_props.header_offset,
     // limit: 5,
     // orderBy: 'todo',
     // query: 'status = TRUE'
